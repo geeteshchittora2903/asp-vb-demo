815df88a9286277d2c190096aaf9fc93819c09be
geeteshchittora2903 <geeteshchittora2903@gmail.com>
2024-12-02 17:02:24 +0530

    demo3.vb
    Signed-off-by: geeteshchittora2903 <geeteshchittora2903@gmail.com>

---
+++ Folder1/demo3.vb
@@ -0,0 +1,10 @@
+Module Program
+    Sub Main()
+        ' Using a For loop to print numbers from 1 to 10
+        For i As Integer = 1 To 10
+            Console.WriteLine(i)
+        Next
+
+        Console.ReadKey()
+    End Sub
+End Module
