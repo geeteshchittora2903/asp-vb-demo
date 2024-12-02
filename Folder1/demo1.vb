4b71ade030654266e8de1afa4f614c72f20ef3b4
geeteshchittora2903 <geeteshchittora2903@gmail.com>
2024-12-02 17:00:04 +0530

    demo1.vb
    Signed-off-by: geeteshchittora2903 <geeteshchittora2903@gmail.com>

---
+++ Folder1/demo1.vb
@@ -0,0 +1,8 @@
+Module Module1
+    Sub Main()
+        ' Display a message to the console
+        Console.WriteLine("Hello, World!")
+        ' Wait for the user to press a key before closing the console
+        Console.ReadKey()
+    End Sub
+End Module
